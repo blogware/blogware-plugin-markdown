04bea3c4f8ef38e128bd143211fe569d940af97d
Leo Wong <gnowoel@gmail.com>
2016-01-12 00:21:04 +0800

    Complete the first version of the plugin

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var type = 'markup converter';
 var extnames = ['.md'];
-
+var compile = require('./lib/compile');
+var route = require('./lib/route');
+var render = require('./lib/render');
 
 exports.type = type;
 exports.extnames = extnames;
+exports.compile = compile;
+exports.route = route;
+exports.render = render;
